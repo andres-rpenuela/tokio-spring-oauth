9aaeac155fb09e9170782baf0e0e245b0972c890
andres.rpenuela <andresruizpenuela@gmail.com>
2025-03-09 16:08:54 +0100

    [aotuh0] created web Auth0 and login google account

---
+++ Oauth0Cleint/src/main/java/com/tokioschool/oauth/oauth0cleint/Oauth0CleintApplication.java
@@ -0,0 +1,13 @@
+package com.tokioschool.oauth.oauth0cleint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Oauth0CleintApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Oauth0CleintApplication.class, args);
+    }
+
+}
---
+++ Oauth0Cleint/src/test/java/com/tokioschool/oauth/oauth0cleint/Oauth0CleintApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tokioschool.oauth.oauth0cleint;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Oauth0CleintApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
